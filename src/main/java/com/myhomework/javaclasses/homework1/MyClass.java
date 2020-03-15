d76129f76cd1853d917a7e20d62141738357cac0
Timur.Burganov <timur.burganov@x5.ru>
2020-03-15 16:58:57 +0300

    добавлены доделки по 3-ему дз

--- src/main/java/com/myhomework/javaclasses/homework1/MyClass.java
+++ src/main/java/com/myhomework/javaclasses/homework1/MyClass.java
@@ -1,6 +1,5 @@
 package com.myhomework.javaclasses.homework1;
 
-import javax.sql.XAConnection;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
@@ -9,57 +8,61 @@ import java.util.stream.Stream;
  */
 public class MyClass {
 
-    public static void doNothing(){
+    public static void doNothing() {
 
     }
 
-    public static int doMultiplyTwo(int a){
+    public static int doMultiplyTwo(int a) {
         int tmp = 0;
-        try{
-            tmp = a  > Integer.MAX_VALUE/2 ? 0: a*2;
-        }catch (ArithmeticException e){
+        try {
+            tmp = Integer.MAX_VALUE / 2;
+            if (a <= tmp) {
+                return a * 2;
+            } else {
+                throw new IllegalArgumentException("Value of " + a + " cannot be multiplied by 2 due to exceed int value");
+            }
+        } catch (ArithmeticException e) {
             System.out.println("Arithmetic exception");
             return a;
         }
-        return tmp;
     }
 
-    public static void showSum(int a, int b){
-        System.out.println(a+b);
+    public static void showSum(int a, int b) {
+        System.out.println(a + b);
     }
 
-    public void methodWorks(){
+    public void methodWorks() {
         System.out.println("it works!");
     }
 
-    public void showStrCountTimes(String str, int count){
-        Stream.iterate(0, i -> ++i).limit(count).forEach(i -> System.out.println(str+" "));
+    public void showStrCountTimes(String str, int count) {
+        Stream.iterate(0, i -> ++i).limit(count).forEach(i -> System.out.println(str + " "));
     }
 
-    public boolean isValueGraterThanZero(int a){
+    public boolean isValueGraterThanZero(int a) {
         return a > 0;
     }
 
-    public void printArray(Object[] objects){
-        Arrays.stream(objects).forEach(e -> System.out.print(e+" "));
+    public void printArray(Object[] objects) {
+        Arrays.stream(objects).forEach(e -> System.out.print(e + " "));
     }
 
-    public boolean isParity(int value){
+    public boolean isParity(int value) {
         return value % 2 == 0;
     }
 
-    public boolean canDevideThree(int value){
+    public boolean canDevideThree(int value) {
         return value % 3 == 0;
     }
 
-    public boolean canDevideFifth(int value){
+    public boolean canDevideFifth(int value) {
         return value % 5 == 0;
     }
 
-    public static int capacity(int value){
+    public static int capacity(int value) {
 
         int capacity = 0;
-        while(value > 0){
+        while (value > 0) {
             capacity++;
             value = value >>> 1;
         }
